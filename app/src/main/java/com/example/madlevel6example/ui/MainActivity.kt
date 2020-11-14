c3ffc28ea701d75c8c5858ac94033f954c245db1
Acdaling <acdaling@gmail.com>
2020-11-15 00:39:23 +0100

    Renamed the fabbutton and added onclick functionality

--- app/src/main/java/com/example/madlevel6example/ui/MainActivity.kt
+++ app/src/main/java/com/example/madlevel6example/ui/MainActivity.kt
@@ -1,23 +1,24 @@
 package com.example.madlevel6example.ui
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import com.example.madlevel6example.R
+import com.example.madlevel6example.ui.viewmodels.TriviaViewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel: TriviaViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        findViewById<FloatingActionButton>(R.id.fabButton).setOnClickListener {
+            viewModel.getTriviaNumber()
         }
     }
 
--- app/src/main/java/com/example/madlevel6example/ui/TriviaFragment.kt
+++ app/src/main/java/com/example/madlevel6example/ui/TriviaFragment.kt
@@ -1,22 +1,25 @@
 package com.example.madlevel6example.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.example.madlevel6example.R
+import com.example.madlevel6example.ui.viewmodels.TriviaViewModel
+import kotlinx.android.synthetic.main.fragment_trivia.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class TriviaFragment : Fragment() {
+    private val viewModel: TriviaViewModel by activityViewModels()
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_trivia, container, false)
@@ -25,6 +28,16 @@ class TriviaFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        observeTrivia()
+    }
 
+    private fun observeTrivia() {
+        viewModel.trivia.observe(viewLifecycleOwner, {
+            tvTrivia.text = it?.text
+        })
+        // Observe the error message.
+        viewModel.errorText.observe(viewLifecycleOwner, {
+            Toast.makeText(activity, it, Toast.LENGTH_SHORT).show()
+        })
     }
 }

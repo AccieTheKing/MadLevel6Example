7622f307d077988ff5dd603137726021a41ec88d
Acdaling <acdaling@gmail.com>
2020-11-14 23:06:41 +0100

    Created ui package and models package

--- app/src/main/java/com/example/madlevel6example/ui/TriviaFragment.kt
+++ app/src/main/java/com/example/madlevel6example/ui/TriviaFragment.kt
@@ -1,4 +1,4 @@
-package com.example.madlevel6example
+package com.example.madlevel6example.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.example.madlevel6example.R
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.

d1840bf8baed437a5ad390055c0e1816b697b776
Acdaling <acdaling@gmail.com>
2020-11-14 23:38:27 +0100

    Created an Api service

---
+++ app/src/main/java/com/example/madlevel6example/services/TriviaApiServices.kt
@@ -0,0 +1,9 @@
+package com.example.madlevel6example.services
+
+import com.example.madlevel6example.models.Trivia
+import retrofit2.http.GET
+
+interface TriviaApiServices {
+    @GET("/random/trivia?json")
+    fun getRandomNumberTrivia(): Trivia
+}
